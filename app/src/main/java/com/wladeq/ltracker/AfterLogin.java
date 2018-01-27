9daf8779097d1a7da31df9e8612de89e28e25acb
wladeq <vlad-i@ukr.net>
2018-01-27 10:09:56 +0100

    Added background, list_view

--- app/src/main/java/com/wladeq/ltracker/AfterLogin.java
+++ app/src/main/java/com/wladeq/ltracker/AfterLogin.java
@@ -6,12 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 
 public class AfterLogin extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_po_login);
-
     }
 //12
     public void qwe(View view) {
