5c1b8e8363214f0f91d3ab03cc4744c7c11ab6f4
wladeq <s28759@student.wsinf.eu>
2018-02-06 07:16:17 +0100

    popup

---
+++ app/src/main/java/com/wladeq/ltracker/InstructorChoose.java
@@ -0,0 +1,23 @@
+package com.wladeq.ltracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by VladyslavIgnatenko on 05.02.2018.
+ */
+
+public class InstructorChoose extends AppCompatActivity{
+    public void ins1(){
+        /*Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);*/
+    }
+    public void ins2(){
+        Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);
+    }
+    public void ins3(){
+        Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);
+    }
+}
