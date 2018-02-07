2309659f3737df53f322c367605aecb6a93e75c1
wladeq <s28759@student.wsinf.eu>
2018-02-07 17:33:25 +0100

    1

--- app/src/main/java/com/wladeq/ltracker/MapsActivity.java
+++ app/src/main/java/com/wladeq/ltracker/MapsActivity.java
@@ -221,11 +221,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                // Prompt the user once explanation has been shown
-
                 ActivityCompat.requestPermissions(this,
                         new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_LOCATION);
