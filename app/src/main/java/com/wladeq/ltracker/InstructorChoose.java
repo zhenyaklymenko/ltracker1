dc3098b299548f7f9d22746f3f337fc848d1c218
wladeq <s28759@student.wsinf.eu>
2018-02-08 21:22:38 +0100

    done

--- app/src/main/java/com/wladeq/ltracker/InstructorChoose.java
+++ app/src/main/java/com/wladeq/ltracker/InstructorChoose.java
@@ -3,11 +3,11 @@ import android.support.v7.app.AppCompatActivity;
 
 //Klas, który dostaje wartość od usera i póżniej ją otdaje
 public class InstructorChoose extends AppCompatActivity{
-    String choice;
+    static String choice;
     public String getChoice() {
         return choice;
     }
     public void setChoice(String choice) {
-        this.choice = choice;
+        InstructorChoose.choice = choice;
     }
 }
